45a1810896c53b4f3c3406abe23c517fbb25a1c4
Alexey <cooldock4play@gmail.com>
2025-06-13 22:14:53 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+type task struct {
+	id          int
+	info        string
+	status      bool
+	createdTime int
+	doneTime    int
+}
+
+func main() {
+	fmt.Println("Hello! Here's your tasks:")
+
+}
+
+// Добавление новой задачи
+func add() {
+
+}
+
+// Изменение статуса задачи
+func changeStatus() {
+
+}
+
+// Удаление задачи
+func delete() {
+
+}
+
+// Вывод всех задач на экран
+func printing() {
+
+}
